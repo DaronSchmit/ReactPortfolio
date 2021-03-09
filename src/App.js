d36946b5e2a974c98c3187776e2d0575761bd3dc
Daron Schmit <darons23@gmail.com>
2021-03-08 20:56:46 -0600

    fixed Home page import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Nav/>
           <Switch>
               <Route exact path="/">
-                <Books />
-              </Route>
-              <Route path="*">
-                <NoMatch />
+                <Home/>
               </Route>
           </Switch>
         </div>
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Home(props){
+    return(
+        <h1>Daron Schmit</h1>
+    )
+}
+
+export default Home;
