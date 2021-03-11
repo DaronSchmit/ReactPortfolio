fd283f2ad867b41d1de51ca7af1dc370e4acd80e
Daron Schmit <darons23@gmail.com>
2021-03-11 00:04:00 -0600

    adjusted text centering on contactme

--- src/pages/ContactMe/index.jsx
+++ src/pages/ContactMe/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ContactHeader from '../../components/ContactHeader';
+import ContactInfo from "../../components/ContactInfo";
+import Resume from "../../components/Resume";
 
 function ContactMe(props){
     return(
         <div> 
             <ContactHeader />
-            
+            <br></br>
+            <div className="row">
+                <Resume/>
+                <ContactInfo/>
+            </div>
         </div>
     )
 }
