c4ad215ea483944597a52e592cca64ac1d2259eb
Daron Schmit <darons23@gmail.com>
2021-03-10 21:38:10 -0600

    polishing footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props){
+    return(
+        <div className="text-center sticky-bottom card-footer bg-dark footer-font-style text-muted footer-position">
+          <p>Copyright Daron Schmit 2021</p>
+        </div>
+        )
+}
+
+export default Footer;
