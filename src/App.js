fc07c9194173e32c2cef298440f25ba8be41a887
Daron Schmit <darons23@gmail.com>
2021-03-10 23:09:51 -0600

    fixed routing for gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './pages/Home';
 import NoMatch from './pages/NoMatch';
 import TopNav from './components/Nav';
@@ -10,25 +10,21 @@ import Footer from "./components/Footer";
 
 
 function App() {
+  console.log(process.env.PUBLIC_URL);
+  
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <TopNav/>
       <br></br>
       <div className="container">
           <Switch>
-              <Route exact path="/">
-                <Home/>
-              </Route>
-              <Route exact path="/Projects">
-                <Projects />
-              </Route>
-              <Route exact path="*">
-                <NoMatch />
-              </Route>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/Projects" component={Projects}/>
+              <Route component={NoMatch} status={404}/>
           </Switch>
       </div>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,8 +6,8 @@ function TopNav(props){
         <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Daron Schmit</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/Projects">Projects</Nav.Link>
+          <Nav.Link href="/ReactPortfolio/#">Home</Nav.Link>
+          <Nav.Link href="/ReactPortfolio/#/Projects">Projects</Nav.Link>
           <Nav.Link href="https://github.com/DaronSchmit">Github</Nav.Link>
           <Nav.Link href="https://linkedin.com/in/Daron-Schmit">LinkedIn</Nav.Link>
         </Nav>
