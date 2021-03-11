9e8cf8f1638959a6382d8b5f10aee589e2051203
Daron Schmit <darons23@gmail.com>
2021-03-11 00:01:10 -0600

    added contact info

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ContactHeader from '../../components/ContactHeader';
+import resume from "../../assets/images/resume.pdf";
 
-function ContactMe(props){
+function Resume(props){
     return(
         <section class="col-sm-12 col-md-8">
             <h4>Here's My resume!</h4>
-            <iframe src="https://github.com/DaronSchmit/ReactPortfolio/blob/main/src/assets/images/Resume%20(1).pdf" width="100%" height="750px">
-            </iframe>
+            <iframe title="resume" src={resume} width="100%" height="750px"></iframe>
         </section>
     )
 }
 
-export default ContactMe;
+export default Resume;
