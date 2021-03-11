94f9c1730b2b7d1ff928f4aa13d65d361d19b559
Daron Schmit <darons23@gmail.com>
2021-03-10 23:36:35 -0600

    added contactme page header

---
+++ src/components/ContactHeader/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactHeader(props){
+    return(
+        <header class="row">
+            <header class = "col-sm-12">
+                <h1>Contact Me!</h1>
+            </header>
+        </header>
+    )
+}
+
+export default ContactHeader;
---
+++ src/pages/ContactMe/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactHeader from '../../components/ContactHeader';
+
+function ContactMe(props){
+    return(
+        <div> 
+            <ContactHeader />
+            
+        </div>
+    )
+}
+
+export default ContactMe;
