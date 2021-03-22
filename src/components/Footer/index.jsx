69e430ad1a24c31ee03e681a4c4fc2cbb56c4a37
Daron Schmit <darons23@gmail.com>
2021-03-22 00:06:17 -0500

    added app.yaml for google cloud deployment

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(props){
     return(
-        <div className="text-center sticky-bottom card-footer bg-dark footer-font-style text-muted footer-position">
+        <div fixed="bottom" className="text-center sticky-bottom card-footer bg-dark footer-font-style text-muted footer-position">
           <p>Copyright Daron Schmit 2021</p>
         </div>
         )
