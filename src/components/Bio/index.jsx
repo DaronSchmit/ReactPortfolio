301742a437cc19acae98c9aeefa609a4d8d4a5f9
Daron Schmit <darons23@gmail.com>
2022-03-26 17:47:15 -0500

    adjusted bio

--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -8,14 +8,15 @@ function Bio(){
             <img src={couplePic} alt="Daron" id="headshot-wrapper" />
             <h2 className="aboutme-heading">Daron Schmit</h2>
             <p>
-            Daron is a 26 year-old Web Developer <strong>(NG normal guy)</strong>. He can be found in an apartment in Minneapolis, brooding at his computer, writing code for money.
+                Daron is a 26 year-old Web Developer <strong>(NG normal guy)</strong>.
+                He can be found in an apartment in Minneapolis, brooding at his computer, writing code for money.
             </p>
             <p>
-            For fun, Daron likes to run Dungeons and Dragons 5e or play whatever Tabletop his friends are Game Mastering. He and his buddy Greg publish a worldbuilding podcast! How neat is that?
+                Oftentimes, Daron is found hunched over his dining room table prepping for DnD or assembling and painting minis for wargames.
+                For social enrichment, Daron can be found at one of his buddies' houses, playing board games or recording his podcast.
             </p>
-
             <p>
-            Otherwise Daron just likes to laze about the house and watch movies, hang out with his girlfriend, play fetch with the dog, and go on vacations when the world isn't ending.
+                Otherwise Daron watches TV with his wife, sharpens his cooking skills, and hangs out with the dog most of the time. 
             </p>
         </article>
     )
