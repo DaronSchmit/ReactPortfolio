ad5e56282beea6eb24819c064bff617bdf30a5fa
Daron Schmit <darons23@gmail.com>
2022-03-26 18:02:00 -0500

    adjusted container to actually work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import './App.css';
-// import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './pages/Home';
-// import NoMatch from './pages/NoMatch';
-// import TopNav from './components/Nav';
-// import Projects from './pages/Projects';
 import Footer from "./components/Footer";
-// import ContactMe from "./pages/ContactMe";
-
 
 function App() {
  
   return (
     <>
-      <div className="container">
+      <div id="container">
         <Home />
       </div>
       <Footer />
